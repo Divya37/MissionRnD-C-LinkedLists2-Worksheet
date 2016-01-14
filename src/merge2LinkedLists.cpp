409e10973a94c86a704e5205dd0941f4d704fc67
Divya37 <Divya37@missionrnd.com>
2016-01-14 19:08:28 +0500

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -18,6 +18,56 @@ struct node {
 	struct node *next;
 };
 
-struct node * merge2LinkedLists(struct node *head1, struct node *head2) {
-	return NULL;
+struct node * merge2LinkedLists(struct node *head1, struct node *head2)
+{
+	struct node *temp, *head;
+	if (head1 == NULL && head2 == NULL)
+		return NULL;
+	if (head1 == NULL)
+		head = head2;
+	else if (head2 == NULL)
+		head = head1;
+	else
+	{
+		if (head1->num < head2->num)
+		{
+			head = head1;
+			head1 = head1->next;
+			temp = head;
+		}
+		else
+		{
+			head = head2;
+			head2 = head2->next;
+			temp = head;
+		}
+		while (head1 != NULL && head2 != NULL)
+		{
+			if (head1->num < head2->num)
+			{
+				temp->next = head1;
+				head1 = head1->next;
+				temp = temp->next;
+			}
+			else
+			{
+				temp->next = head2;
+				head2 = head2->next;
+				temp = temp->next;
+			}
+		}
+		while (head1 != NULL)
+		{
+			temp->next = head1;
+			head1 = head1->next;
+			temp = temp->next;
+		}
+		while (head2 != NULL)
+		{
+			temp->next = head2;
+			head2 = head2->next;
+			temp = temp->next;
+		}
+	}
+	return head;
 }
